4a212b98fc03f47573993a56c84ee795ee74d8b5
Aashir120 <60323401+Aashir120@users.noreply.github.com>
2020-09-23 12:40:44 +0500

    Add files via upload

---
+++ build/precache-manifest.27b1190024e58865aa9cb6445ae006d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba02aa34ea3de97c0f4ca52e04a1e62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab34082f54ec16e4844",
+    "url": "/static/css/main.78455201.chunk.css"
+  },
+  {
+    "revision": "26ad800b266d67236043",
+    "url": "/static/js/2.09d1831e.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.09d1831e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab34082f54ec16e4844",
+    "url": "/static/js/main.5aacbc92.chunk.js"
+  },
+  {
+    "revision": "35f166285d10ea98ed95",
+    "url": "/static/js/runtime-main.c1438df8.js"
+  },
+  {
+    "revision": "d2865ff9f56d055a8dea3171a2ac3a25",
+    "url": "/static/media/01.d2865ff9.png"
+  }
+]);
